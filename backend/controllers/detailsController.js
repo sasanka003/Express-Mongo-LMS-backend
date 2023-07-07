b687a078ae2c869f662c461728e1b6695ef8c563
sasanka udana <sasankaudana@gmail.com>
2023-07-07 12:08:51 +0530

    added details controllers

--- backend/controllers/detailsController.js
+++ backend/controllers/detailsController.js
@@ -1,15 +1,19 @@
 const asyncHandler = require('express-async-handler')
 
+const Student = require('../models/studentModel')
+
 // Description  Get details
 // Route        GET /api/details
 // Access       Private
 const getDetails = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get details' })
+    const student = await Student.find()
+
+    res.status(200).json(student)
 })
 
 // Description  Set details
 // Route        POST /api/details
-// Access       Private
+// Access       Private 
 const setDetails = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         //res.status(400).json({ message: 'Please add a text' })
@@ -17,21 +21,47 @@ const setDetails = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({ message: 'Set details' })
+    const student = await Student.create({
+        text: req.body.text,
+    })
+
+    res.status(200).json(student)
 })
 
 // Description  Update details
 // Route        PUT /api/details/:id
 // Access       Private
 const updateDetails = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update details ${req.params.id}` })
+    const student = await Student.findById(req.params.id)
+
+    if(!student) {
+        res.status(400)
+        throw new Error('Student not found')
+    }
+
+    const updatedStudent = await Student.findByIdAndUpdate(req.params.id, req.body,
+        {
+            new: true,
+        })
+    // res.status(200).json({ message: `Update details ${req.params.id}` })
+    res.status(200).json(updatedStudent)
 })
 
 // Description  Delete details
 // Route        DELETE /api/details/:id
 // Access       Private
 const deleteDetails = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete details ${req.params.id}` })
+    const student = await Student.findById(req.params.id)
+
+    if(!student) {
+        res.status(400)
+        throw new Error('Student not found')
+    }
+
+    await student.remove
+
+    res.status(200).json({ id: req.params.id })
+    //res.status(200).json({ message: `Delete details ${req.params.id}` })
 })
 
 module.exports = {
